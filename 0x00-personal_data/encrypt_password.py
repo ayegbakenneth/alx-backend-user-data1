1f019aa52fbba2327746f80075b853e4202d9367
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-03 14:53:18 +0000

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" File executable path """
+
+import bcrypt
+""" Module importation path """
+
+
+def hash_password(password):
+    """ A function that returns a hashed password """
+    pass_word = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pass_word, bcrypt.gensalt())
