480f4652e255970005d0f5c2136de82881d689cd
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-07 09:49:31 +0000

    New update on basic authentication concept

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,10 +13,39 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+if ken_env_var == "RIGHT_AUTH_TYPE":
+    auth = RightAuthType()
+elif not_ken_env_var == "DIFFERENT_AUTH_TYPE":
+    auth = DifferentAuthType
+
+if auth:
+    from api.v1.auth.auth import Auth
+auth_instance = Auth()
+auth = auth_instance
+
+
+@app.before_request
+def before_request():
+    """ A method to handle the before_request """
+    if auth is None:
+        return
+
+
+not_included = [
+        '/api/v1/status/', '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+if request.path not in not_included and auth.require_auth(
+        request.path, not_included):
+    return
+if auth.authorization_header(request) is None:
+    abort(401)
+if auth.current_user(request) is None:
+    abort(403)
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
+    """ A not found error method """
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
@@ -24,11 +53,13 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
+    """ An unauthorized method handler """
     return jsonify({"error": "unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
+    """ A forbidden error method """
     return jsonify({"error": "Forbidden"})
 
 
