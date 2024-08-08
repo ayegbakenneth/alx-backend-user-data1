482f45155041673ce9343ba4b620bb3ddae90bd0
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-08 11:13:20 +0000

    New update on the basic authentication concept

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ File executable path """
 
-from api.v1.auth import Auth
+from api.v1.auth.auth import Auth
 """ Module importation path """
 
 
 class BasicAuth(Auth):
+    pass
