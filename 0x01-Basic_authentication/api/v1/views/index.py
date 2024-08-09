87e69f87377ed611f50074d47035d84bb41bfa23
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-09 16:05:45 +0000

    New corrections on the codes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,11 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
-def unauthorized_endpoint():
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error.
+    """
     abort(401)
 
 
-@app_views.route('/forbidden', methods='[GET'])
-def forbidden_endpoint():
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
     abort(403)
